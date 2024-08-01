9fafb764407a5bc108f6dde3b1b1de723125833b
eliyas41 <fentaeliyas@gmail.com>
2024-08-01 07:54:30 +0300

    isLoggedIn middleware

--- server/routes/usersRoute.js
+++ server/routes/usersRoute.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { registerUserCtrl, loginUserCtrl, getUserProfileCtrl } from '../controllers/usersCtrl.js';
+import { isLoggedIn } from '../middlewares/isLoggedIn.js';
 
 const userRoutes = express.Router();
 
 userRoutes.post("/register", registerUserCtrl);
 userRoutes.post("/login", loginUserCtrl);
-userRoutes.get('/profile', getUserProfileCtrl)
+userRoutes.get('/profile', isLoggedIn, getUserProfileCtrl)
 
 
 export default userRoutes;
