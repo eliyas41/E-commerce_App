f32dff2a3b41e814f1435c906ebe6731d99f48b5
eliyas41 <fentaeliyas@gmail.com>
2024-08-01 05:12:32 +0300

    Hash user password

--- server/controllers/usersCtrl.js
+++ server/controllers/usersCtrl.js
@@ -1,5 +1,5 @@
 import User from "../model/User.js";
-
+import bcrypt from 'bcryptjs'
 // @desc    Register user
 // @route   POST /api/v1/users/register
 // @access  Private/Admin
@@ -17,11 +17,13 @@ export const registerUserCtrl = async (req, res) => {
   }
 
   // hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
   // Create the user
   const user = await User.create({
     fullname,
     email,
-    password
+    password: hashedPassword,
   });
   res.status(201).json({
     status: "success",
