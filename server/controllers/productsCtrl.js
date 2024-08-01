2ee6c3bca8f1a7c6e1ee224e272f49378bf9fd90
eliyas41 <fentaeliyas@gmail.com>
2024-08-01 16:34:47 +0300

    Filter products by name

--- server/controllers/productsCtrl.js
+++ server/controllers/productsCtrl.js
@@ -35,7 +35,18 @@ export const createProductCtrl = asyncHandler(async (req, res) => {
 // @route   GET /api/v1/products
 // @access  Public
 export const getProductsCtrl = asyncHandler(async (req, res) => {
-  const products = await Product.find();
+  // console.log(req.query);
+  let productQuery = Product.find();
+
+  // Search by name 
+  if (req.query.name) {
+    productQuery = productQuery.find({
+      name: { $regex: req.query.name, $options: 'i' },
+    });
+  };
+  // await the query
+  const products = await productQuery;
+
   res.json({
     status: "success",
     products,
