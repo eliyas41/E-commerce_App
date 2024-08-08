4f0f04db05419be1888bde6b78109c7632936445
eliyas41 <fentaeliyas@gmail.com>
2024-08-08 17:25:04 +0300

    Coupon Validations

--- server/model/Coupon.js
+++ server/model/Coupon.js
@@ -27,7 +27,53 @@ const CouponSchema = new Schema(
       required: true,
     },
   },
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+  },
 );
+
+//coupon is expired
+CouponSchema.virtual("isExpired").get(function () {
+  return this.endDate < Date.now();
+});
+
+// CouponSchema.virtual("daysLeft").get(function () {
+//   const daysLeft =
+//     Math.ceil((this.endDate - Date.now()) / (1000 * 60 * 60 * 24)) +
+//     " " +
+//     "Days left";
+//   return daysLeft;
+// });
+
+//validation
+CouponSchema.pre("validate", function (next) {
+  if (this.endDate < this.startDate) {
+    next(new Error("End date cannot be less than the start date"));
+  }
+  next();
+});
+
+CouponSchema.pre("validate", function (next) {
+  if (this.startDate < Date.now()) {
+    next(new Error("Start date cannot be less than today"));
+  }
+  next();
+});
+
+CouponSchema.pre("validate", function (next) {
+  if (this.endDate < Date.now()) {
+    next(new Error("End date cannot be less than today"));
+  }
+  next();
+});
+
+CouponSchema.pre("validate", function (next) {
+  if (this.discount <= 0 || this.discount > 100) {
+    next(new Error("Discount cannot be less than 0 or greater than 100"));
+  }
+  next();
+});
 
 const Coupon = mongoose.model("Coupon", CouponSchema);
 
